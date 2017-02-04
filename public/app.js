be6996c674511ea8cfc141c1c59b75c92836a097
kathrynehlmann <kathryn@wagnermediaworks.com>
2017-02-04 14:29:56 -0500

    Data showing on Frontend

--- public/app.js
+++ public/app.js
@@ -1,14 +1,18 @@
 var app = angular.module('acquired', []);
 
 app.controller('mainController', ['$http', function($http) {
-  this.message = "controller is working";
-  this.notices = [];
+  // this.message = "controller is working";
+  this.artworks = [];
 
   $http({
     method: 'GET',
-    url: 'http://localhost3000/users',
+    url: 'http://localhost:3000/artworks',
   }).then(function(response) {
     console.log(response);
-  });
+    this.artworks = response.data;
+    console.log(this.artworks);
+  }.bind(this));
 
 }]);
+
+// external api request will be updated URLs and Heroku server, as well as rack cors so that the origin matches the frontend url.
