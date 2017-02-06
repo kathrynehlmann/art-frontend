a987f3ac94088ce2dfa65c521f4a03915dc829ee
kathrynehlmann <kathryn@wagnermediaworks.com>
2017-02-06 15:40:01 -0500

    fixed bug in app.js controller

--- public/app.js
+++ public/app.js
@@ -1,18 +1,24 @@
-var app = angular.module('acquired', ['ngRoute', 'appRoutes', 'MainCtrl', 'SettingsCtrl', 'SettingsService']);
+var app = angular.module('acquired', []);
 
 app.controller('mainController', ['$http', function($http) {
-  // this.message = "controller is working";
-  this.artworks = [];
+  this.message = "controller is working"; // controller is working
+  this.artworks = []; // ger array of artworks json data
+  var controller = this;
 
+
+// this will break when pushed to heroku, need to adjust after deploy
   $http({
     method: 'GET',
-    url: 'localhost:3000/artworks',
+    url: 'http://localhost:3001/artworks',
   }).then(function(response) {
     console.log(response);
     this.artworks = response.data;
     console.log(this.artworks);
   }.bind(this));
 
-}]);
+
+  
+
+}]); //end MainController
 
 // external api request will be updated URLs and Heroku server, as well as rack cors so that the origin matches the frontend url.
--- server.js
+++ server.js
@@ -1,17 +1,27 @@
+//Dependencies
 var express = require('express');
 var app     = express();
+var bodyParser = require('body-parser');
+var port       = 3001;
 
-// var mongoose = require('mongoose');
+//===========================================
+//Middleware
+app.use(bodyParser.urlencoded({ extended:false })); //req.body
+app.use(bodyParser.json()); // req.bodyParser and use JSON
+//===========================================
+
+
+
+//port
+port = process.env.PORT || 3001;
+
 // this threw an error Cannot find module 'pg':
 // var pg = require('pg')
-
+// var mongoose = require('mongoose');
+// mongoose.connect(mongoUri);
 
 
 // var mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
-
-// mongoose.connect(mongoUri);
-
-port = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 
