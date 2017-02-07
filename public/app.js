54524e1b68808d346901b7ab9802deb56fd71a70
kathrynehlmann <kathryn@wagnermediaworks.com>
2017-02-06 19:24:46 -0500

    Resolved CORS and GET error

--- public/app.js
+++ public/app.js
@@ -7,18 +7,26 @@ app.controller('mainController', ['$http', function($http) {
 
 
 // this will break when pushed to heroku, need to adjust after deploy
+// app.controller('mainController', ['$http', function($http){
+//   this.message = "mainController is connected"
+//   var controller = this;
+//   console.log(controller);
+//   this.url = 'http://localhost:3000'
+
+
+///requesting the information from the backend, always the localhost address for the port on the backend.
   $http({
     method: 'GET',
-    url: 'http://localhost:3001/artworks',
+    //change this for heroku deployment to back end heroku URL.
+    url: '//localhost:3000',
   }).then(function(response) {
     console.log(response);
     this.artworks = response.data;
     console.log(this.artworks);
   }.bind(this));
 
-
-  
-
 }]); //end MainController
 
 // external api request will be updated URLs and Heroku server, as well as rack cors so that the origin matches the frontend url.
+
+// url:'https://acquired.herokuapp.com/'
--- server.js
+++ server.js
@@ -2,18 +2,10 @@
 var express = require('express');
 var app     = express();
 var bodyParser = require('body-parser');
-var port       = 3001;
-
-//===========================================
-//Middleware
-app.use(bodyParser.urlencoded({ extended:false })); //req.body
-app.use(bodyParser.json()); // req.bodyParser and use JSON
-//===========================================
+// var mongoose = require('mongoose')
+var port       =  3001 || process.env.PORT;
 
 
-
-//port
-port = process.env.PORT || 3001;
 
 // this threw an error Cannot find module 'pg':
 // var pg = require('pg')
@@ -21,11 +13,11 @@ port = process.env.PORT || 3001;
 // mongoose.connect(mongoUri);
 
 
-// var mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+// var mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/acquired';
 
 app.use(express.static('public'));
 
 
-app.listen(3001, function(){
-  console.log("Acquired frontend is running on port: ", port);
+app.listen(port, function(){
+  console.log('Acquired frontend is listening on port: ', port);
 });
