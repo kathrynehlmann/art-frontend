bae79aa9dd6acbf024191df6658358f362728e6e
kathrynehlmann <kathryn@wagnermediaworks.com>
2017-02-07 16:37:41 -0500

    artworks only

--- public/app.js
+++ public/app.js
@@ -1,12 +1,17 @@
 var app = angular.module('acquired', []);
 
 app.controller('mainCtrl', ['$http', function($http) {
-  this.message = "controller is working"; // controller is working
+  // this.message = "controller is working"; // controller is working
   this.artworks = []; // get array of artworks json data
+  this.artwork = [];
   this.users = []; //get array of user json data
+  this .formcreateuser = {};
+  this.deleteUsers = [];
 
 
 ///requesting the information from the backend, always the localhost address for the port on the backend. This is the GET request that displays the list of artworks from the Rails server.
+
+////////////////   request Artworks  ///////////////////////////
   $http({
     method: 'GET',
     //change this for heroku deployment to back end heroku URL. acquired-api.herokuapp.com from '//localhost:3000'
@@ -15,18 +20,36 @@ app.controller('mainCtrl', ['$http', function($http) {
     console.log(response);
     this.artworks = response.data;
     console.log(this.artworks);
-  }.bind(this));
+  }.bind(this)); //end artwork request
 
   ///requesting the information from the backend. This is the GET request that displays the list of users from the Rails server.
-    $http({
-      method: 'GET',
-      //change this for heroku deployment to back end heroku URL. acquired-api.herokuapp.com from '//localhost:3000'
-      url: 'https://acquired-api.herokuapp.com/', //backend url only
-    }).then(function(response) {
-      console.log(response);
-      this.users = response.data;
-      console.log(this.users);
-    }.bind(this));
+
+  ///////////////////  request Users    //////////////////
+    // $http({
+    //   method: 'GET',
+    //   //change this for heroku deployment to back end heroku URL. acquired-api.herokuapp.com from '//localhost:3000'
+    //   url: 'https://acquired-api.herokuapp.com/', //backend url only
+    // }).then(function(response) {
+    //   console.log(response);
+    //   this.users = response.data;
+    //   console.log(this.users);
+    // }.bind(this));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
