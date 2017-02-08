7ffc34a71edfd291f8e283d91c2fa598490111a3
kathrynehlmann <kathryn@wagnermediaworks.com>
2017-02-07 21:42:24 -0500

    faker data only enabled

--- public/app.js
+++ public/app.js
@@ -10,51 +10,51 @@ app.controller('mainCtrl', ['$http', function($http) {
   this.favorites = [];
 
 
-/////////////  request Artworks from arst.net API /////////////
-////  Added Superagent as a dependency in server.js ///////
-
-var clientID = '5df977ee477d20875d23',
-    clientSecret = 'c359841e6f964922a22c47efcea3639e',
-    apiUrl = 'https://api.artsy.net/api/tokens/xapp_token'
-    xappToken;
-
-request
-  .post(apiUrl)
-  .send({ client_id: clientID, client_secret: clientSecret })
-  .end(function(res) {
-    xappToken = res.body.token;
-  });
-
-////////////////   request Artworks from API  ///////////////////
-  $http({
-    method: 'GET',
-    url: 'https://api.artsy.net/api/artworks?sample=1', //external api url only, call is for a random image
-  }).then(function(response) {
-    console.log(response);
-    this.artworks = response.data;
-    console.log(this.artworks);
-  }.bind(this)); //end artwork request
-
-  //also with this from https://developers.artsy.net - Traverston-angular npm package installed.
-  var traverson = require('traverson'),
-    JsonHalAdapter = require('traverson-hal'),
-    xappToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0ODcxMDg4NTQsImlhdCI6MTQ4NjUwNDA1NCwiYXVkIjoiNTg5MzkwZTY5YzE4ZGIyODc0ODU4ZjA0IiwiaXNzIjoiR3Jhdml0eSIsImp0aSI6IjU4OWE0MDc2MTM5YjIxNTVhNWViODlhOSJ9.PElXi7HVIei9hz8X9hWLbQha0fIa4sOHjrLPq9wBzXk';
-
-  traverson.registerMediaType(JsonHalAdapter.mediaType, JsonHalAdapter);
-  api = traverson.from('https://api.artsy.net/api').jsonHal();
-
-  api.newRequest()
-  .follow('artist')
-  .withRequestOptions({
-    headers: {
-      'X-Xapp-Token': xappToken,
-      'Accept': 'application/vnd.artsy-v2+json'
-    }
-  })
-  .withTemplateParameters({ id: 'andy-warhol' })
-  .getResource(function(error, andyWarhol) {
-    console.log(andyWarhol.name + 'was born in ' + andyWarhol.birthday + ' in ' + andyWarhol.hometown);
-  });
+// /////////////  request Artworks from arsty.net API /////////////
+// ////  Added Superagent as a dependency in server.js ///////
+//
+// var clientID = '5df977ee477d20875d23',
+//     clientSecret = 'c359841e6f964922a22c47efcea3639e',
+//     apiUrl = 'https://api.artsy.net/api/tokens/xapp_token'
+//     xappToken;
+//
+// request
+//   .post(apiUrl)
+//   .send({ client_id: clientID, client_secret: clientSecret })
+//   .end(function(res) {
+//     xappToken = res.body.token;
+//   });
+//
+// ////////////////   request Artworks from API  ///////////////////
+//   $http({
+//     method: 'GET',
+//     url: 'https://api.artsy.net/api/artworks?sample=1', //external api url only, call is for a random image
+//   }).then(function(response) {
+//     console.log(response);
+//     this.artworks = response.data;
+//     console.log(this.artworks);
+//   }.bind(this)); //end artwork request
+//
+//   //also with this from https://developers.artsy.net - Traverston-angular npm package installed.
+//   var traverson = require('traverson'),
+//     JsonHalAdapter = require('traverson-hal'),
+//     xappToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0ODcxMDg4NTQsImlhdCI6MTQ4NjUwNDA1NCwiYXVkIjoiNTg5MzkwZTY5YzE4ZGIyODc0ODU4ZjA0IiwiaXNzIjoiR3Jhdml0eSIsImp0aSI6IjU4OWE0MDc2MTM5YjIxNTVhNWViODlhOSJ9.PElXi7HVIei9hz8X9hWLbQha0fIa4sOHjrLPq9wBzXk';
+//
+//   traverson.registerMediaType(JsonHalAdapter.mediaType, JsonHalAdapter);
+//   api = traverson.from('https://api.artsy.net/api').jsonHal();
+//
+//   api.newRequest()
+//   .follow('artist')
+//   .withRequestOptions({
+//     headers: {
+//       'X-Xapp-Token': xappToken,
+//       'Accept': 'application/vnd.artsy-v2+json'
+//     }
+//   })
+//   .withTemplateParameters({ id: 'andy-warhol' })
+//   .getResource(function(error, andyWarhol) {
+//     console.log(andyWarhol.name + 'was born in ' + andyWarhol.birthday + ' in ' + andyWarhol.hometown);
+//   });
 
 ///requesting the information from the backend, always the localhost address for the port on the backend. This is the GET request that displays the list of artworks from the Rails server.
 
