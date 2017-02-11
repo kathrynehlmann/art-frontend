d3d0a546c26f8dbffbb4a1208d047cf3aab9deb7
kathrynehlmann <kathryn@wagnermediaworks.com>
2017-02-11 16:02:28 -0500

    New user form is now functional, all Ajax calls load without error

--- public/app.js
+++ public/app.js
@@ -10,7 +10,7 @@ app.controller('mainCtrl', ['$http', function($http) {
   // this.deleteUsers = [];
   // this.favorites = []; //get a user's array of favorited artworks
 
-  var localEnv = false; //change to true if using localhost, change to false if on heroku
+  var localEnv = true; //change to true if using localhost, change to false if on heroku
   if (localEnv) { var urlString = 'http://localhost:3000/'} else { var urlString = 'https://acquired-api.herokuapp.com/' }
 
 
@@ -46,14 +46,14 @@ app.controller('mainCtrl', ['$http', function($http) {
 //
 // //     ///////////////////  Create New Users    //////////////////
 // //
-    this.processFormforUser = function() {
-      console.log('processFormforUser function . . .');
+    this.processFormForUser = function() {
+      console.log('processFormForUser function . . .');
       console.log('Form: ', this.formcreateuser);
 //
 //       //Ajax request to create a user
       $http({
         method: 'POST',
-        url: 'urlString' + '/users',
+        url: urlString + '/users',
         data: this.formcreateuser
       }).then(function(result){
         console.log('data from server: ', result);
@@ -61,53 +61,53 @@ app.controller('mainCtrl', ['$http', function($http) {
         this.users.unshift(result.data);
       }.bind(this)); //end user create
     } //end form create user
-// // //
+
 // // //       ///////////////////  Delete User    //////////////////
-// // //
+
 // // //       // JS function to delete users
-//       this.deleteUsers = function(users) {
-//         for (i = 0; i < this.users.length; i++) {
-//           if( this.users[i].id == users.id) {
-//             this.users.splice(i,1);
-//           }
-//         }
-//       } //end of JS function to delete users
+      this.deleteUsers = function(users) {
+        for (i = 0; i < this.users.length; i++) {
+          if( this.users[i].id == users.id) {
+            this.users.splice(i,1);
+          }
+        }
+      } //end of JS function to delete users
 // // //
 // // //       //Delete users from database in Rails
-//       this.deleteUsersFromDB = function(userId) {
-//         console.log(usersId);
+      this.deleteUsersFromDB = function(userId) {
+        console.log(usersId);
 // //
 // //       //Ajax request to to delete an individual user
-//       $http({
-//         method: 'DELETE',
-//         url: 'urlString' + '/users' + users.id,
-//       }).then(function(response) {
-//         console.log(response);
-//         this.deleteUsers(response.data);
-//         console.log(this.deleteUsers);
-//       });// end request to delete an individual user
-//     } //end delete users from database function
+      $http({
+        method: 'DELETE',
+        url: urlString + '/users' + users.id,
+      }).then(function(response) {
+        console.log(response);
+        this.deleteUsers(response.data);
+        console.log(this.deleteUsers);
+      });// end request to delete an individual user
+  } //end delete users from database function
 
 
 ///-------AJAX calls to Favorites for create  and update ---//
 //
 // // //////////////  add a favorite to a user's collection ///////////////
- // this.favorites = function(index){
- //   $http({
- //     method: 'POST',
- //     url: urlString + '/users/favorites',
- //     data: this.favorites
- //   }).then(
- //     function(response){ //success message if artwork is favorited.
- //     console.log('Creating Favorite');
- //     console.log(response);
- //   },
- //    function(response){ //fail message if favorite is not added
- //      console.log('Warning: favorite was not added!');
- //      console.log(response);
- //      }
- //   )
- // };
+ this.favorites = function(index){
+   $http({
+     method: 'POST',
+     url: urlString + '/users/favorites',
+     data: this.favorites
+   }).then(
+     function(response){ //success message if artwork is favorited.
+     console.log('Creating Favorite');
+     console.log(response);
+   },
+    function(response){ //fail message if favorite is not added
+      console.log('Warning: favorite was not added!');
+      console.log(response);
+      }
+   )
+ };
 //
 // //then a delete function for favorites
 //
